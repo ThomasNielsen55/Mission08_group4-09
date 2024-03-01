463fbf7d443db73e74a5a35db5e59247b3f86df2
jimy-goose <131339722+jimy-goose@users.noreply.github.com>
2024-03-01 16:36:32 -0700

    Data

--- Models/ToDoList.cs
+++ Models/ToDoList.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Mission08_group4_09.Models;
 
 public partial class ToDoList
 {
+    [Key]
     public int TaskId { get; set; }
 
     public string Task { get; set; } = null!;
@@ -13,7 +16,9 @@ public partial class ToDoList
 
     public int Quadrant { get; set; }
 
+    [ForeignKey("CategoryId")]
     public int? CategoryId { get; set; }
+    public Category? Category { get; set; }
 
     public int? Completed { get; set; }
 }
